a9fc2d9fdce7502b08c8413cabb6fcf6eecbdf55
Greg Skinner <gds@alum.mit.edu>
2021-04-07 23:09:37 -0700

    Update Number in Words.C

--- Recursion programs/Number in Words.C
+++ Recursion programs/Number in Words.C
@@ -3,6 +3,8 @@
 #include<stdio.h>
 void f(int n); 
 
+const char* nums[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
 int main() 
 { 
 	int num = 12340;
@@ -16,17 +18,5 @@ void f(int n)
 
 	f(n/10);
 
-	switch(n%10)
-	{
-		case 0: printf("zero ");break;
-		case 1: printf("one ");break;
-		case 2: printf("two ");break;
-		case 3: printf("three ");break;
-		case 4: printf("four ");break;
-		case 5: printf("five ");break;
-		case 6: printf("six ");break;
-		case 7: printf("seven ");break;
-		case 8: printf("eight ");break;
-		case 9: printf("nine ");break;
-	}
+	printf("%s ", nums[n%10]);
 } 
